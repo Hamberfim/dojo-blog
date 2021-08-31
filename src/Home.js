bbb27138061b8a92b56f9d0714ce1d0ab82e5e75
Anthony <adhamlingithub@gmail.com>
2021-08-31 11:40:51 -0500

    click, click events

--- src/Home.js
+++ src/Home.js
@@ -24,8 +24,7 @@ const Home = () => {
         alert(`Hello ${name}!`, e);
     }
 
-
-
+    // JSX return template 
     return ( 
         <div className="home">
             <h3>Home Page - Content Header - h3</h3>
