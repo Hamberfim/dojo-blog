b8903378d258ef3f1ee66659e9f78fea1785a59b
Anthony <adhamlingithub@gmail.com>
2021-09-01 13:17:10 -0500

    reactive value change using 'useState' hook

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
-const Home = () => {
+import {useState} from 'react';
 
+const Home = () => {
+    // array destructuring for useState Hook
+    const [name, setName] = useState('Little Bill');  // made reactive ( or like kockout.js it's observable)
+    // array destructuring - value = age, the function = setAge
+    const [age, setAge] = useState(25);
+;
     // home page button click
     const handleClick = () => {
-        console.log("Hello User!");
-        alert("Hello User!");
+        // useState array destructuring Function Call
+        setName('Big Bill');
+        setAge(55);
     }
 
 
@@ -11,7 +18,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <h3>Home Page - Content Header - h3</h3>
-            <p>Lorem ipsum dolor sit amet. Et ullamQuo reprehenderit?</p>
+            <p className="bigBill">{name} is {age} years old.</p>
             {/* Button - can't invoke the function i.e., handleClick() on page load */}
             <button className="button" onClick={handleClick}>Click Me</button>
             <br />
