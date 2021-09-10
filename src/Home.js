3e46b77b15b8bb7efa3dafa88503ffffdf7ebfc1
Anthony <adhamlingithub@gmail.com>
2021-09-10 11:10:27 -0500

    setting up json-server

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,13 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {id: 0, title: 'Long Dought Fermentation', copy: 'Lorem ipsum...', author: 'Bill'},
-        {id: 1, title: 'What is a Pre-Ferment', copy: 'Lorem ipsum...', author: 'Sarah'},
-        {id: 2, title: 'Four, Water, Salt', copy: 'Lorem ipsum...', author: 'Bill'},
-        {id: 3, title: 'Bench Tools', copy: 'Lorem ipsum...', author: 'Sarah'},
-        {id: 4, title: 'Your Bakehouse', copy: 'Lorem ipsum...', author: 'Bill'},
-        {id: 5, title: 'Which Stand Mixer is right for You', copy: 'Lorem ipsum...', author: 'Sarah'},
-        {id: 6, title: 'Home Milling Flour', copy: 'Lorem ipsum...', author: 'Sarah'},
-        {id: 7, title: "When to use '00' flour", copy: 'Lorem ipsum...', author: 'Sarah'},
-        {id: 8, title: 'Room Temperature Fermentation', copy: 'Lorem ipsum...', author: 'Bill'},
-        {id: 9, title: 'Stand Mixer Dough Hooks', copy: 'Lorem ipsum...', author: 'Sarah'}
-    ]);
-
-    const [name, setName] = useState('Kendra');
+    // hard coded data  moved to ./data/db.json (json-server >>) npx json-server --watch src/data/db.json --port 8000
+    // const [blogs, setBlogs] = useState([
+    //     {id: 0, title: 'Long Dought Fermentation', copy: 'Lorem ipsum...', author: 'Bill'},
+    //     {id: 1, title: 'What is a Pre-Ferment', copy: 'Lorem ipsum...', author: 'Sarah'},
+    //     {id: 2, title: 'Four, Water, Salt', copy: 'Lorem ipsum...', author: 'Bill'},
+    //     {id: 3, title: 'Bench Tools', copy: 'Lorem ipsum...', author: 'Sarah'}
+    // ]);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -25,12 +18,11 @@ const Home = () => {
     // useEffect with an anonymous function - runs every time there is a re-render
     useEffect(() => {
         console.log("UseEffect ran");
-        console.log(name);
         // fetch data or auth - run a side effect - changing state in here could create a infinity loop
 
         // empty dependancy array - if only need the func to run once on the intitial render
         // add 'name' to the dependancy array so that the useEffect runs when the button is clicked
-    }, [name]);
+    }, []);
 
     // JSX return template 
     return ( 
@@ -38,10 +30,6 @@ const Home = () => {
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
             <BlogList blogs={blogs.filter((blog) => blog.author === 'Bill')} title="Bill's Blogs" />
             <BlogList blogs={blogs.filter((blog) => blog.author === 'Sarah')} title="Sarah's Blogs" />
-
-            {/* name change useState/useEffect denpendancy */}
-            <button onClick={() => setName('Tommy')}>change name</button>
-            <p>{name}</p>
             
         </div>
      );
