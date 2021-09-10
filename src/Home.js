a150326931c1edcbb0d665d99e28eddef7202cfb
Anthony <adhamlingithub@gmail.com>
2021-09-10 10:51:09 -0500

    state changes and useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -15,10 +15,22 @@ const Home = () => {
         {id: 9, title: 'Stand Mixer Dough Hooks', copy: 'Lorem ipsum...', author: 'Sarah'}
     ]);
 
+    const [name, setName] = useState('Kendra');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    // useEffect with an anonymous function - runs every time there is a re-render
+    useEffect(() => {
+        console.log("UseEffect ran");
+        console.log(name);
+        // fetch data or auth - run a side effect - changing state in here could create a infinity loop
+
+        // empty dependancy array - if only need the func to run once on the intitial render
+        // add 'name' to the dependancy array so that the useEffect runs when the button is clicked
+    }, [name]);
 
     // JSX return template 
     return ( 
@@ -26,6 +38,11 @@ const Home = () => {
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
             <BlogList blogs={blogs.filter((blog) => blog.author === 'Bill')} title="Bill's Blogs" />
             <BlogList blogs={blogs.filter((blog) => blog.author === 'Sarah')} title="Sarah's Blogs" />
+
+            {/* name change useState/useEffect denpendancy */}
+            <button onClick={() => setName('Tommy')}>change name</button>
+            <p>{name}</p>
+            
         </div>
      );
 }
